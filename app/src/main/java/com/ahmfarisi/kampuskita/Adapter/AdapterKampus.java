853767af6d49d6c99d7539a83b1d9b4d51a73034
miadiviaa <miadivia@mhs.mdp.ac.id>
2023-05-31 12:49:10 +0700

    yiha
    yiha

--- app/src/main/java/com/ahmfarisi/kampuskita/Adapter/AdapterKampus.java
+++ app/src/main/java/com/ahmfarisi/kampuskita/Adapter/AdapterKampus.java
@@ -1,18 +1,30 @@
 package com.ahmfarisi.kampuskita.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ahmfarisi.kampuskita.API.APIRequestData;
+import com.ahmfarisi.kampuskita.API.RetroServer;
+import com.ahmfarisi.kampuskita.Activity.MainActivity;
+import com.ahmfarisi.kampuskita.Activity.UbahActivity;
 import com.ahmfarisi.kampuskita.Model.ModelKampus;
+import com.ahmfarisi.kampuskita.Model.ModelResponse;
 import com.ahmfarisi.kampuskita.R;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdapterKampus extends RecyclerView.Adapter<AdapterKampus.VHKampus> {
     private Context ctx;
@@ -46,6 +58,7 @@ public class AdapterKampus extends RecyclerView.Adapter<AdapterKampus.VHKampus> 
 
     public class VHKampus extends RecyclerView.ViewHolder{
         TextView tvId, tvNama, tvKota, tvAlamat;
+        Button btnHapus, btnUbah;
 
         public VHKampus(@NonNull View itemView) {
             super(itemView);
@@ -54,6 +67,56 @@ public class AdapterKampus extends RecyclerView.Adapter<AdapterKampus.VHKampus> 
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvKota = itemView.findViewById(R.id.tv_kota);
             tvAlamat = itemView.findViewById(R.id.tv_alamat);
+            btnHapus = itemView.findViewById(R.id.btn_hapus);
+            btnUbah = itemView.findViewById(R.id.btn_ubah);
+
+            btnHapus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteKampus(tvId.getText().toString());
+                }
+            });
+
+            btnUbah.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent pindah = new Intent(ctx, UbahActivity.class);
+                    pindah.putExtra("xId", tvId.getText().toString());
+                    pindah.putExtra("xNama", tvNama.getText().toString());
+                    pindah.putExtra("xKota", tvKota.getText().toString());
+                    pindah.putExtra("xAlamat", tvAlamat.getText().toString());
+                    ctx.startActivity(pindah);
+
+
+
+                }
+            });
+
+        }
+
+        void deleteKampus(String id){
+            APIRequestData API = RetroServer.konekRetrofit().create(APIRequestData.class);
+            Call<ModelResponse> proses = API.ardDelete(id);
+
+            proses.enqueue(new Callback<ModelResponse>() {
+                @Override
+                public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                    String kode = response.body().getKode();
+                    String pesan = response.body().getPesan();
+
+                    Toast.makeText(ctx, "Kode : " + kode + "Pesan : " + pesan, Toast.LENGTH_SHORT).show();
+                    ((MainActivity) ctx).retrieveKampus();
+
+                }
+
+                @Override
+                public void onFailure(Call<ModelResponse> call, Throwable t) {
+                    Toast.makeText(ctx, "Error! Gagal Menghubungi Server!", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+
         }
     }
 
